458bf515bd500784dcdab75592daf01e3a2b8d27
northonh <nobile.pedro@gmail.com>
2022-10-19 19:51:26 -0300

    Implementação de Adapter sem utilizar viewBinding.

--- app/src/main/java/br/edu/ifsp/ads/pdm/mycontacts/adapter/ContactAdapter.kt
+++ app/src/main/java/br/edu/ifsp/ads/pdm/mycontacts/adapter/ContactAdapter.kt
@@ -8,7 +8,6 @@ import android.view.ViewGroup
 import android.widget.ArrayAdapter
 import android.widget.TextView
 import br.edu.ifsp.ads.pdm.mycontacts.R
-import br.edu.ifsp.ads.pdm.mycontacts.databinding.TileContactBinding
 import br.edu.ifsp.ads.pdm.mycontacts.model.Contact
 
 class ContactAdapter(
@@ -16,24 +15,27 @@ class ContactAdapter(
     private val contactList: MutableList<Contact>
 ) : ArrayAdapter<Contact>(context, R.layout.tile_contact, contactList) {
     private data class TileContactHolder(val nameTv: TextView, val emailTv: TextView)
-    private lateinit var tcb: TileContactBinding
 
     override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
         val contact = contactList[position]
         var contactTileView = convertView
         if (contactTileView == null) {
             // Inflo uma nova célula
-            tcb = TileContactBinding.inflate(
-                context.getSystemService(LAYOUT_INFLATER_SERVICE) as LayoutInflater,
-                parent,
-                false
-            )
+            contactTileView =
+                (context.getSystemService(LAYOUT_INFLATER_SERVICE) as LayoutInflater).inflate(
+                    R.layout.tile_contact,
+                    parent,
+                    false
+                )
 
-            contactTileView = tcb.root
-            val tileContactHolder = TileContactHolder(tcb.nameTv, tcb.emailTv)
+            val tileContactHolder = TileContactHolder(
+                contactTileView.findViewById(R.id.nameTv),
+                contactTileView.findViewById(R.id.emailTv),
+            )
             contactTileView.tag = tileContactHolder
         }
-        with (contactTileView.tag as TileContactHolder) {
+
+        with(contactTileView?.tag as TileContactHolder) {
             nameTv.text = contact.name
             emailTv.text = contact.email
         }
